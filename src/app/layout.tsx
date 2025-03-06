ba8d5b153f87aa9088fded70e99b3fdbaf8841a8
willsather <will@sather.ws>
2025-03-06 14:33:45 -0500

    chore: improve favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import { ReactNode } from "react";
+
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Sather Family",
   description: "Created by Will Sather, for the Sather family",
+  icons: [{ rel: "icon", url: "/favicon.svg", type: "image/svg+xml" }],
 };
 
 export default function RootLayout({
